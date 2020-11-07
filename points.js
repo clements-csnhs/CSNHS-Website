54afa7cc4ceff7426ee6def8837072d323df530d
Great-The-Nate <natemustafanam@gmail.com>
2020-11-06 21:31:42 -0600

    Reformatting files

--- points.js
+++ points.js
@@ -6,7 +6,7 @@ function queryPoints(){
 
 	var xml = new XMLHttpRequest();
     var result = null;
-    xml.open("GET", "points.txt", false);
+    xml.open("GET", "CSNHSPoints.json", false);
     xml.send();
     if (xml.status==200) {
       result = JSON.parse(xml.responseText);
