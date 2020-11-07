adad4c3207a1840f9fc352e85fa4eb1f40c2e608
Great-The-Nate <natemustafanam@gmail.com>
2020-11-06 23:35:20 -0600

    Fixing point query

--- points.js
+++ points.js
@@ -3,8 +3,7 @@ function queryPoints(){
 	//var fname = document.getElementById("point-query-fname").value.toUpperCase();
     //var lname = document.getElementById("point-query-lname").value.toUpperCase();
     var sid = document.getElementById("point-query-sid").value;
-    console.log(sid);
-    if(sid=="" || sid==null)
+    if(sid=="")
     	alert("Please fill in all fields");
 
 	var xml = new XMLHttpRequest();
@@ -14,13 +13,17 @@ function queryPoints(){
     if (xml.status==200) {
       result = JSON.parse(xml.responseText);
     }
+    //var result = JSON.parse('[{"id":760481,"name":"Nathan Mustafa","points":5},{"id":123456,"name":"Test User","points":123}]');
     console.log(result);
 
+    console.log(sid);
     var user;
-    for(var temp in result)
-    	if(temp["id"]==sid)
-    		user=temp;
+    for(var i=0; i<result.length; i++)
+    	if(result[i]["id"]==sid){
+    		user=result[i];
+    		break;
+    	}
 
     console.log("Member: "+user["name"]+"\nPoints: "+user["points"])
-    document.getElementById("point-query-info").innerHTML = "Member: "+user["name"]+"\nPoints: "+user["points"];
+    document.getElementById("point-query-info").innerHTML = "Member: "+user["name"]+"<br/>Points: "+user["points"];
 }
